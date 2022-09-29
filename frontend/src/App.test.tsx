a4168a8ec1dbc6c7fc1f6434c02d50f2187929f6
Daniel Knerr <knerr@sipgate.de>
2022-09-29 13:56:09 +0200

    :white_check_mark: add tests for App.tsx

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,10 +1,39 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
+import * as API from "./api";
+import { Ranking } from "./api";
 
-// TODO: write useful tests
-test("renders rankings header", () => {
+test("renders the phone number", async () => {
+  const last4Digits = "1234";
+  const ranking = [
+    {
+      duration: 1,
+      hasWon: 2,
+      tries: 3,
+      usersTel: "+xxxxxxx" + last4Digits,
+    },
+  ] as Ranking[];
+
+  jest.spyOn(API, "getRankings").mockReturnValue(Promise.resolve(ranking));
+
   render(<App />);
-  const linkElement = screen.getByText(/Rankings/i);
-  expect(linkElement).toBeInTheDocument();
+  await waitFor(() => {
+    const linkElement = screen.getByText((text) => {
+      return text.includes(last4Digits);
+    });
+    expect(linkElement).toBeInTheDocument();
+  });
+});
+
+test("renders an error when getRankings fails", async () => {
+  const errMessage = "abc";
+  jest
+    .spyOn(API, "getRankings")
+    .mockReturnValue(Promise.reject(new Error(errMessage)));
+
+  render(<App />);
+  await waitFor(() => {
+    const linkElement = screen.getByText(`Error: ${errMessage}`);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
